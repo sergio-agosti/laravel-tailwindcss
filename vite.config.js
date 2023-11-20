8151dce3d84a3ade912cd6e52b377e2662124af2
Sergio Agosti <sagosti@theframeworks.com>
2023-11-20 15:42:24 +0000

    Config Vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const { APP_URL } = loadEnv(mode, process.cwd(), 'APP_');
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+                detectTls: new URL(APP_URL).hostname,
+            }),
+        ],
+    };
 });
