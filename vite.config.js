20515fb2b315f006c55e0ba4ba32e4465d142381
Sergio Agosti <sagosti@theframeworks.com>
2023-11-28 10:13:13 +0000

    Only use detectTls when needed

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig, loadEnv } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig, loadEnv } from "vite";
+import laravel from "laravel-vite-plugin";
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig(({ mode }) => {
-    const { APP_URL } = loadEnv(mode, process.cwd(), 'APP_');
+    const { APP_URL } = loadEnv(mode, process.cwd(), "APP_");
+    const appUrl = new URL(APP_URL);
 
     return {
         plugins: [
             laravel({
-                input: ['resources/css/app.css', 'resources/js/app.js'],
+                input: ["resources/css/app.css", "resources/js/app.js"],
                 refresh: true,
-                detectTls: new URL(APP_URL).hostname,
+                detectTls: appUrl.protocol === "https:" && appUrl.hostname,
             }),
         ],
     };
